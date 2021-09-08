fc0ef81ec6f3cd34aee56ab5514fe9e615e7916f
Manuel Munoz <mannymunoz1223@live.com>
2021-09-08 14:19:08 -0600

    Added BestAdderEver class

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author Manny
+ */
+public class BestAdderEver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        float num3 = 1.25f;
+        
+        System.out.println(num1 + " + " + num2 + " + " + num3 + " = " + (num1+num2+num3));
+    }
+    
+}
